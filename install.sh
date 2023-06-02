3b7a74abc9cdc2c48fc60dcb21df0fde55bad606
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-03 00:30:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,8 +96,8 @@ clear
 echo "Installing customizations"
 sleep 2
 
-plymouth-set-default-theme fade-in
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/' /etc/default/grub
+plymouth-set-default-theme spinner
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash splash-delay=7000"/' /etc/default/grub
 git clone https://github.com/vinceliuice/grub2-themes.git
 cd grub2-themes
 chmod +x install.sh
