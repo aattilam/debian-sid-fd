ea65aea396491a0ad0d4f583fab0acc42d048b8d
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-04 15:35:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,12 +50,12 @@ EOT
 clear
 echo "Installing gnome and default software"
 sleep 2
+dpkg --add-architecture i386
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install gnome-core --no-install-recommends -y
 apt install libreoffice libreoffice-gnome gnome-tweaks gnome-initial-setup dconf-cli dirmngr software-properties-gtk flatpak network-manager gnome-software-plugin-flatpak chrome-gnome-shell intel-microcode amd64-microcode plymouth plymouth-themes git nala vlc qgnomeplatform-qt5 adwaita-qt adwaita-qt6 firmware-linux-nonfree firefox fonts-crosextra-carlito fonts-crosextra-caladea firmware-misc-nonfree ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi -y
-dpkg --add-architecture i386
-apt install wine winetricks -y
+apt install winetricks wine wine32 wine64 libwine libwine:i386 fonts-wine -y
 
 echo "Configuring Networking"
 
